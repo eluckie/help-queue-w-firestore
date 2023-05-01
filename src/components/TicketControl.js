d29464fc1d3d533e5b26883d9f82f5d8a068fa34
E Luckie <eluckie.d@gmail.com>
2023-05-01 14:41:26 -0600

    add view functionality using firebase db

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,13 +4,46 @@ import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
 import db from "./../firebase.js";
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, onSnapshot } from 'firebase/firestore';
+import { useEffect } from 'react';
 
 function TicketControl() {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, "tickets"),
+      (collectionSnapshot) => {
+        // .data() transforms ALL of a documents data into javascript object so you shorten code using spread operator
+        // ALSO using .map() instead of forEach
+
+        // const tickets = collectionSnapshot.docs.map((doc) => {
+        //   return {
+        //     ...doc.data(),
+        //     id: doc.id
+        //   };
+        // });
+        const tickets = [];
+        collectionSnapshot.forEach((doc) => {
+          tickets.push({
+            names: doc.data().names,
+            location: doc.data().location,
+            issue: doc.data().issue,
+            id: doc.id
+          });
+        });
+        setMainTicketList(tickets);
+      },
+      (error) => {
+        setError(error.message);
+      }
+    );
+    return () => unSubscribe();
+  }, []);
 
   const handleClick = () => {
     if (selectedTicket != null) {
@@ -54,7 +87,9 @@ function TicketControl() {
   let currentlyVisibleState = null;
   let buttonText = null; 
 
-  if (editing ) {      
+  if (error) {
+    currentlyVisibleState = <p>There was an error: {error} </p>
+  } else if (editing ) {      
     currentlyVisibleState = <EditTicketForm
       ticket = {selectedTicket}
       onEditTicket = {handleEditingTicketInList} />
@@ -78,7 +113,7 @@ function TicketControl() {
   return (
     <React.Fragment>
       {currentlyVisibleState}
-      <button onClick={handleClick}>{buttonText}</button> 
+      {error ? null : <button onClick={handleClick}>{buttonText}</button> }
     </React.Fragment>
   );
 }
