0ec6d52beb418ec64ba492579e45dee652aed468
E Luckie <eluckie.d@gmail.com>
2023-05-01 16:06:13 -0600

    add authentication - sign up, sign in, & sign out functionality

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,7 +3,7 @@ import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
-import db from "./../firebase.js";
+import { db } from "./../firebase.js";
 import { collection, addDoc, onSnapshot, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 import { useEffect } from 'react';
 
