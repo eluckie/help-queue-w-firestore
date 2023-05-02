52388add0a674069c6778dab3f316bab59477a1e
E Luckie <eluckie.d@gmail.com>
2023-05-01 18:00:15 -0600

    add styled component to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  background-color: purple;
+`
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
+      <HelpQueueHeader>
+        Help Queue
+      </HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
